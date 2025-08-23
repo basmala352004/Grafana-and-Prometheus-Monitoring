b07be870ebe63cee8909d678e200add105109f12
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2025-08-23 14:39:25 +0300

    add proj

---
+++ monitoring/solar-system-app/app.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const express = require('express');
+const OS = require('os');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const client = require('prom-client');
+const app = express();
+const cors = require('cors')
+
+// Initialize Prometheus metrics
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+// Custom metric: count HTTP requests
+const httpRequests = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "statusCode"],
+});
+register.registerMetric(httpRequests);
+
+// Middleware to increment counter
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequests.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      statusCode: res.statusCode,
+    });
+  });
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/')));
+app.use(cors())
+
+mongoose.connect(process.env.MONGO_URI, {
+    user: process.env.MONGO_USERNAME,
+    pass: process.env.MONGO_PASSWORD,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function(err) {
+    if (err) {
+        console.log("error!! " + err)
+    } else {
+      //  console.log("MongoDB Connection Successful")
+    }
+})
+
+var Schema = mongoose.Schema;
+
+var dataSchema = new Schema({
+    name: String,
+    id: Number,
+    description: String,
+    image: String,
+    velocity: String,
+    distance: String
+});
+var planetModel = mongoose.model('planets', dataSchema);
+
+
+
+app.post('/planet',   function(req, res) {
+   // console.log("Received Planet ID " + req.body.id)
+    planetModel.findOne({
+        id: req.body.id
+    }, function(err, planetData) {
+        if (err) {
+            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
+            res.send("Error in Planet Data")
+        } else {
+            res.send(planetData);
+        }
+    })
+})
+
+app.get('/',   async (req, res) => {
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+
+app.get('/os',   function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "os": OS.hostname(),
+        "env": process.env.NODE_ENV
+    });
+})
+
+app.get('/live',   function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "status": "live"
+    });
+})
+
+app.get('/ready',   function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send({
+        "status": "ready"
+    });
+})
+
+// Expose metrics at /metrics
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
+app.listen(3000, () => {
+    console.log("Server successfully running on port - " +3000);
+})
+
+
+module.exports = app;
